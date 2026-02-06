0add7a3cb9bc80756fb93f7294977e98fa1c959d
rico-coder <rico_sonstiges@icloud.com>
2026-02-06 11:38:39 +0100

    add class Main, School and Subject with average methods

--- Lernporjekt-GitHub/src/Main.java
+++ Lernporjekt-GitHub/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projekt läuft!");
+        School bms = new School("BMS");
+
+        Subject mathe = new Subject("Mathe");
+        Subject chemie = new Subject("Chemie");
+        bms.addSubject(mathe);
+        bms.addSubject(chemie);
+
+        mathe.addGrade(5.5f);
+        mathe.addGrade(4.5f);
+        mathe.addGrade(4.5f);
+
+        chemie.addGrade(5.0f);
+        chemie.addGrade(5.0f);
+        chemie.addGrade(4.0f);
+
+
+
+        System.out.println("Schule: " + bms.name);
+        System.out.println("---");
+
+        for (Subject subject : bms.subjects) {
+            System.out.println("Fach: " + subject.name);
+            System.out.println("Noten: " + subject.grades);
+            float roundGradeAverage = subject.average();
+            System.out.println("Durchschnitt: " + roundGradeAverage);
+            System.out.println("---");
+        }
+        float bmsAverage = bms.schoolAverage();
+        System.out.println("Gesamtdurchschnitt: " + bmsAverage);
+
     }
 }
