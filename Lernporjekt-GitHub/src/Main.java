12afda6d5c69286cf820fbffed8776be364737ed
rico-coder <rico_sonstiges@icloud.com>
2026-02-12 15:04:13 +0100

    add scanner, menu, add fach and end program

--- Lernporjekt-GitHub/src/Main.java
+++ Lernporjekt-GitHub/src/Main.java
@@ -1,34 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        School bms = new School("BMS");
 
-        Subject mathe = new Subject("Mathe");
-        Subject chemie = new Subject("Chemie");
-        bms.addSubject(mathe);
-        bms.addSubject(chemie);
+        Scanner scanner = new Scanner(System.in);
+        School school = new School("BBW");
 
-        mathe.addGrade(5.5f);
-        mathe.addGrade(4.5f);
-        mathe.addGrade(4.5f);
+        boolean running = true;
 
-        chemie.addGrade(5.0f);
-        chemie.addGrade(5.0f);
-        chemie.addGrade(4.0f);
+        while (running) {
 
+            System.out.println("\n--- MENÜ ---");
+            System.out.println("1 - Fach hinzufügen");
+            System.out.println("2 - Note hinzufügen");
+            System.out.println("3 - Alle Fächer anzeigen");
+            System.out.println("4 - Gesamtdurchschnitt anzeigen");
+            System.out.println("0 - Beenden");
 
+            int choice = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Schule: " + bms.name);
-        System.out.println("---");
+            if (choice == 1) {
+                System.out.print("Name des Fachs: ");
+                String subjectName = scanner.nextLine();
 
-        for (Subject subject : bms.subjects) {
-            System.out.println("Fach: " + subject.name);
-            System.out.println("Noten: " + subject.grades);
-            float roundGradeAverage = subject.average();
-            System.out.println("Durchschnitt: " + roundGradeAverage);
-            System.out.println("---");
+                Subject subject = new Subject(subjectName);
+                school.addSubject(subject);
+
+                System.out.println("Fach hinzugefügt.");
+
+            }  else if (choice == 0) {
+                running = false;
+                System.out.println("Programm beendet.");
+            }
         }
-        float bmsAverage = bms.schoolAverage();
-        System.out.println("Gesamtdurchschnitt: " + bmsAverage);
 
+        scanner.close();
     }
 }
